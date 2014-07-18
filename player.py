649ef5eff5966ceb6f1d44505a351f06f2c66307
FilipHarald <Filip.harald@gmail.com>
2014-07-18 14:54:10 +0200

    player.py

--- player.py
+++ player.py
@@ -6,17 +6,22 @@ class Player(db.Model):
     nick = db.StringProperty(required=True)
     char_code = db.StringProperty(required=True)
     matches_won = db.IntegerProperty(required=True,default=0)
+
 #returns null if user not found
+
+
 def get_matches_won(user):
     q = db.GlQuery("SELECT matches_won FROM Player WHERE user = '%s'" % user)
     m = q.get()
     return m
+
 
 def add_match_won(user):
     q = db.GlQuery("SELECT * FROM Player WHERE user = '%s'" % user)
     u = q.get()
     u.matches_won += 1
     u.put()
+
 
 def reset_matches_won(user):
     q = db.GlQuery("SELECT * FROM Player WHERE user = '%s'" % user)
